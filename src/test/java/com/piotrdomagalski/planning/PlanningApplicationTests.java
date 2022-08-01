6a44cea466f504dc97df24df2b1d98cd5646f856
Piotr Domagalski <piotr.domagalski@yahoo.com>
2022-08-01 08:43:20 +0200

    initial commit

---
+++ src/test/java/com/piotrdomagalski/planning/PlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrdomagalski.planning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
