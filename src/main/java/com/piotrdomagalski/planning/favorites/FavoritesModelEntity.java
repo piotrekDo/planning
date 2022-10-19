04e81dcddfcdaa6e60a618aa9d5752467b01b7db
Piotr Domagalski <piotr.domagalski@yahoo.com>
2022-10-19 18:06:02 +0200

    entity created

---
+++ src/main/java/com/piotrdomagalski/planning/favorites/FavoritesModelEntity.java
@@ -0,0 +1,2 @@
+package com.piotrdomagalski.planning.favorites;public class FavoritesModelEntity {
+}
---
+++ src/main/java/com/piotrdomagalski/planning/favorites/FavoritesRepository.java
@@ -0,0 +1,2 @@
+package com.piotrdomagalski.planning.favorites;public interface FavoritesRepository {
+}
